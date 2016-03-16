ddfd6c63ae7dba2e794ed734f5aeffa60b8d2807
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-03-16 19:48:32 +0100

    added DataDate class

---
+++ src/data/entity/datanumber/DataDate.java
@@ -0,0 +1,33 @@
+package data.entity.datanumber;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+/**
+ * Wrapper class for LocalDate
+ * @author Benjamin Held
+ */
+public class DataDate extends DataNumber<LocalDate> {
+    public DataDate(LocalDate attribute) {
+        super(attribute);
+    }
+
+    public LocalDate increment() {
+        attribute = attribute.plus( Period.ofYears(1));
+        return attribute;
+    }
+    public LocalDate decrement() {
+        attribute = attribute.minus(Period.ofYears(1));
+        return attribute;
+    }
+
+    public double getDelta(LocalDate value) {
+        int delta_year = attribute.minus(Period.ofYears(value.getYear())).getYear();
+        int delta_day = attribute.minus(Period.ofDays(value.getDayOfYear())).getDayOfYear();
+        return (delta_year + delta_day / 365.25);
+    }
+
+    public DataDate clone() {
+        return new DataDate(attribute);
+    }
+}
