d404763dc69711eb58b71d504a3834926c0c424a
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-04-17 21:29:48 +0200

    added DataSet class

---
+++ src/data/entity/dataset/DataSet.java
@@ -0,0 +1,19 @@
+package data.entity.dataset;
+
+/**
+ * Basic entity class to represent a dataset.
+ * @author Benjamin Held.
+ */
+public abstract class DataSet<T> {
+    private T[][] values;
+
+    public DataSet(T[][] data) {
+        this.values = data;
+    }
+
+    public T getValueAtPosition(int i, int j) {
+        return values[i][j];
+    }
+
+    public abstract void print();
+}
