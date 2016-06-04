21c6ea73ef92395f361a1ca51c636bfd7bc3c0dd
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-06-04 08:30:12 +0200

    adjusted dataset hierarchy - the generic parameter now extends DataNumber - added code to determine extreme values - added method to retrieve the extreme values

--- src/data/entity/dataset/DataSet.java
+++ src/data/entity/dataset/DataSet.java
@@ -1,21 +1,36 @@
 package data.entity.dataset;
 
+import data.entity.datanumber.DataNumber;
+
 /**
  * Basic entity class to represent a dataset.
  * @author Benjamin Held (04-17-2016)
- * @version 0.1.0
- * @since 05-25-2016
+ * @version 0.2.0
+ * @since 06-04-2016
  */
-public abstract class DataSet<T> {
+public abstract class DataSet<T extends DataNumber> {
     T[][] values;
+    T maxValue;
+    T minValue;
 
     public DataSet(T[][] data) {
         this.values = data;
+        determineExtrema();
     }
 
     public T getValueAtPosition(int i, int j) {
         return values[i][j];
     }
 
+    public T getMaxValue() {
+        return maxValue;
+    }
+
+    public T getMinValue() {
+        return minValue;
+    }
+
     public abstract void print();
+
+    abstract void determineExtrema();
 }
--- src/data/entity/dataset/DataSetDouble.java
+++ src/data/entity/dataset/DataSetDouble.java
@@ -1,23 +1,41 @@
 package data.entity.dataset;
 
+import data.entity.datanumber.DataDouble;
+
 /**
  * Entity class to implement a double dataset.
  * @author Benjamin Held (05-15-2016)
- * @version 0.1.0
- * @since 05-25-2016
+ * @version 0.2.0
+ * @since 06-04-2016
  */
-public class DataSetDouble extends DataSet<Double>{
+public class DataSetDouble extends DataSet<DataDouble>{
 
-    public DataSetDouble(Double[][] data) {
+    public DataSetDouble(DataDouble[][] data) {
         super(data);
     }
 
     public void print() {
-        for (Double[] row : values) {
-            for (Double value : row) {
+        for (DataDouble[] row : values) {
+            for (DataDouble value : row) {
                 System.out.print(value + ", ");
             }
             System.out.println();
+        }
+    }
+
+    @Override
+    void determineExtrema() {
+        maxValue = values[0][0];
+        minValue = values[0][0];
+        for (DataDouble[] row: values) {
+            for (DataDouble value: row) {
+                if (value.isGreater(maxValue)) {
+                    maxValue = value;
+                }
+                if (value.isLesser(minValue)) {
+                    minValue = value;
+                }
+            }
         }
     }
 }
