0b0a4cdfa96296cf1c24d5b541eb1913e8974f72
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-06-07 18:16:10 +0200

    added main class

---
+++ src/run/Runner.java
@@ -0,0 +1,19 @@
+package run;
+
+import data.repository.DataRepository;
+
+/**
+ * Main program to start the application
+ * @author Benjamin Held (05-08-2015)
+ * @since 06-07-2016
+ * @version 0.1.0
+ */
+public class Runner {
+    public static void main(String[] args) {
+        DataRepository repository = DataRepository.getInstance();
+        repository.addDataSeries("/path/to/csv");
+
+        System.out.println("Finished.");
+    }
+}
+
