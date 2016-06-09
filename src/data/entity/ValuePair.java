3a7da2d18131d51f0868f1f1198360668371bd94
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-06-09 19:23:26 +0200

    added ValuePair class

---
+++ src/data/entity/ValuePair.java
@@ -0,0 +1,25 @@
+package data.entity;
+
+/**
+ * Helper entity to create a key-value object.
+ * @author Benjamin Held (04-24-2016)
+ * @since 06-09-2016
+ * @version 0.1.0
+ */
+public class ValuePair<K,V> {
+    private K key;
+    private V value;
+
+    public ValuePair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+}
