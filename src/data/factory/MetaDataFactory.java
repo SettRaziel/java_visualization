e6d13cbc2f6a03bfc88d63cfaa983f1026ab3eb6
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-07-04 16:10:12 +0200

    adjusted error Handling - replaced Exceptions by MetaDataException - added error handling when creating DataTypes

--- src/data/factory/MetaDataFactory.java
+++ src/data/factory/MetaDataFactory.java
@@ -6,6 +6,7 @@ import data.entity.MetaData;
 import data.entity.datanumber.DataDate;
 import data.entity.datanumber.DataDouble;
 import data.entity.datanumber.DataInteger;
+import exception.MetaDataException;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -14,18 +15,18 @@ import java.util.ArrayList;
 /**
  * Static factory class to create meta data from given input data
  * @author Benjamin Held (05-15-2016)
- * @version 0.1.0
- * @since 05-27-2016
+ * @version 0.1.1
+ * @since 07-04-2016
  */
 public class MetaDataFactory {
-    public static MetaData parseMetaData(ArrayList<String[]> input) throws IllegalArgumentException {
+    public static MetaData parseMetaData(ArrayList<String[]> input) throws MetaDataException {
         return new MetaData(createDataType(input.get(0)), // data type
                             createDomain(input.get(1)),   // domain in x
                             createDomain(input.get(2)),   // domain in y
                             createDomain(input.get(3)));  // domain in z
     }
 
-    private static DataDomain createDomain(String[] line) {
+    private static DataDomain createDomain(String[] line) throws MetaDataException {
         switch (line[0]) {
             case "Double":
                 return new DataDomain<>(
@@ -46,11 +47,15 @@ public class MetaDataFactory {
                         new DataDate(LocalDate.parse(line[3].trim(), DateTimeFormatter.ofPattern("dd.MM.yyyy"))),
                         Integer.parseInt(line[4].trim()));
             default:
-                throw new IllegalArgumentException("Error while parsing metadata: no valid type.");
+                throw new MetaDataException("Error while parsing metadata: no valid type for data domain.");
         }
     }
 
-    private static DataType createDataType(String[] line) {
-        return new DataType(line[0].trim(), line[1].trim(), line[2].trim());
+    private static DataType createDataType(String[] line) throws MetaDataException {
+        try {
+            return new DataType(line[0].trim(), line[1].trim(), line[2].trim());
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            throw new MetaDataException("Not enough arguments to create valid meta data information.");
+        }
     }
 }
