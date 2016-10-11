d44a685e1a93e497470bc8f9a4ebff845542256b
Benjamin Held <benjamin.held@stud.hs-hannover.de>
2016-10-11 15:08:47 +0200

    added menu item for data selection

---
+++ src/gui/menu/menuitems/SelectDataSeriesButton.java
@@ -0,0 +1,24 @@
+package gui.menu.menuitems;
+
+import gui.windows.SeriesSelectionWindow;
+
+import javax.swing.*;
+
+/**
+ * menu button to select one of the processed data series
+ * @author Benjamin Held (08-24-2015)
+ * @since 08-24-2016
+ * @version 0.1.0
+ */
+public class SelectDataSeriesButton extends JMenuItem {
+
+    public SelectDataSeriesButton() {
+        super("Select DataSeries");
+        this.addActionListener(e -> {
+
+            SeriesSelectionWindow seriesSelectionWindow = new SeriesSelectionWindow();
+            seriesSelectionWindow.pack();
+            seriesSelectionWindow.setVisible(true);
+        });
+    }
+}
